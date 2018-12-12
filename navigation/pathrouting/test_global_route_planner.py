16b38340ec6925e927cfc3bd0d4ff2f4fe7a0779
Praveen Kumar <pravinblaze@hotmail.com>
2018-12-12 17:51:17 +0530

    PEP8 check

--- navigation/pathrouting/test_global_route_planner.py
+++ navigation/pathrouting/test_global_route_planner.py
@@ -15,25 +15,6 @@ class Test_GlobalRoutePlanner(unittest.TestCase):
         # == Utilities test instance without DAO == #
         self.simple_grp = GlobalRoutePlanner(None)
 
-        # == Instance with sample DAO for graph testing == #
-        input_topology = [[(1, 3), (1, 2)],
-                          [(1, 2), (2, 2)],
-                          [(2, 2), (2, 1)],
-                          [(2, 1), (4, 1)],
-                          [(4, 1), (4, 3)],
-                          [(4, 3), (1, 3)],
-                          [(4, 3), (1, 2)]]
-        dao = GlobalRoutePlannerDAO(None)
-        dao.get_topology = Mock(return_value=input_topology)
-        dao.get_next_waypoint = Mock()
-        wplookup = {(1.0, 2.5): (1.0, 2.0), (1.5, 2.0): (2.0, 2.0),
-                    (2.0, 1.5): (2.0, 1.0), (3.0, 1.0): (4.0, 1.0),
-                    (4.0, 2.0): (4.0, 3.0), (2.5, 3.0): (1.0, 3.0),
-                    (2.5, 2.5): (1, 2.0)}
-        dao.get_next_waypoint.side_effect = lambda *arg: wplookup[arg[0]]
-        self.dao_grp = GlobalRoutePlanner(dao)
-        self.dao_grp.setup()
-
         # == Integration test instance == #
         client = carla.Client('localhost', 2000)
         world = client.get_world()
@@ -48,62 +29,34 @@ class Test_GlobalRoutePlanner(unittest.TestCase):
         self.integ_grp = None
         pass
 
-    def test_plan_route_town01(self):
+    def test_plan_route(self):
         """
         Test for GlobalROutePlanner.plan_route()
-        Run this test with a carla (0.9.1) instance running Town01
+        Run this test with carla server running Town03
         """
         plan = self.integ_grp.plan_route((-60, -5), (-77.65, 72.72))
         self.assertEqual(
             plan, ['START', 'LEFT', 'LEFT', 'GO_STRAIGHT', 'LEFT', 'STOP'])
 
+    def test_path_search(self):
+        """
+        Test for GlobalRoutePlanner.path_search()
+        Run this test with carla server running Town03
+        """
+        self.integ_grp.path_search((191.947, -5.602), (78.730, -50.091))
+        self.assertEqual(
+            self.integ_grp.path_search((196.947, -5.602), (78.730, -50.091)),
+            [256, 157, 158, 117, 118, 59, 55, 230])
+
     def test_localise(self):
         """
-        Test for GlobalROutePlanner.localise()
+        Test for GlobalRoutePlanner.localise()
+        Run this test with carla server running Town03
         """
-        input_topology = [[(0, 1), (1, 0)],
-                          [(2, 1), (3, 1)],
-                          [(1, 1), (2, 1)],
-                          [(2, 1), (1, 2)],
-                          [(2, 2), (3, 2)]]
-        x, y = (1.2, 1.01)
-        nxt_vertex = self.simple_grp.localise(x, y, input_topology)[0]
-        self.assertEqual(nxt_vertex, (1, 1))
-
-    # def test_localise_town01(self):
-    #     """
-    #     Test for GlobalROutePlanner.localise()
-    #     Run this test with a carla (0.9.1) instance running Town01
-    #     """
-    #     x, y = 334.7, 25
-    #     segment = self.integ_grp.localise(x, y, self.integ_grp.topology)
-    #     self.assertEqual(segment, [(334.6214904785156, 3.790151834487915),
-    #                                (334.63958740234375, 53.424442291259766)])
-
-    def test_build_graph(self):
-        """
-        Test for GlobalROutePlanner.build_graph()
-        """
-        graph, id_map = self.dao_grp.build_graph()
-
-        def connection_check(n1, n2):
-            return graph.has_edge(n1, n2)
-
-        self.assertEqual(len(id_map), 6)
-        self.assertTrue(connection_check(0, 1))
-        self.assertTrue(connection_check(1, 2))
-        self.assertTrue(connection_check(2, 3))
-        self.assertTrue(connection_check(3, 4))
-        self.assertTrue(connection_check(4, 5))
-        self.assertTrue(connection_check(5, 0))
-        self.assertTrue(connection_check(5, 1))
-
-    def test_distance_to_line(self):
-        """
-        Test for GlobalROutePlanner.distance_to_line()
-        """
-        dist = self.simple_grp.distance_to_line((0, 0), (2, 2), (2, 0))
-        self.assertEqual(round(dist, 3), round(math.sqrt(2), 3))
+        x, y = (200, -250)
+        segment = self.integ_grp.localise(x, y)
+        self.assertEqual(self.integ_grp.id_map[segment['entry']], 5)
+        self.assertEqual(self.integ_grp.id_map[segment['exit']], 225)
 
     def test_unit_vector(self):
         """
@@ -129,8 +82,6 @@ def suite():
     suite = unittest.TestSuite()
     suite.addTest(Test_GlobalRoutePlanner('test_unit_vector'))
     suite.addTest(Test_GlobalRoutePlanner('test_dot'))
-    suite.addTest(Test_GlobalRoutePlanner('test_distance_to_line'))
-    suite.addTest(Test_GlobalRoutePlanner('test_build_graph'))
     suite.addTest(Test_GlobalRoutePlanner('test_localise'))
     suite.addTest(Test_GlobalRoutePlanner('test_path_search'))
     suite.addTest(Test_GlobalRoutePlanner('test_plan_route'))
