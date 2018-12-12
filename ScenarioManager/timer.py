f6afe714f006189c69869e6c97e71fb9ec69f24c
Fabian Oboril <fabian.oboril@intel.com>
2018-12-12 07:20:50 +0100

    Updated copyright notice in python files
    Change-Id: I73794822495974e32a1620ab972bbc2bf6c1eb45

--- ScenarioManager/timer.py
+++ ScenarioManager/timer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+# Copyright (c) 2018 Intel Labs.
+# authors: Fabian Oboril (fabian.oboril@intel.com)
 #
 # This work is licensed under the terms of the MIT license.
 # For a copy, see <https://opensource.org/licenses/MIT>.
