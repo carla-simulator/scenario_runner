35f7718cbd426ecd40e4f687f6b38865dfdcf4bd
Subhashini Perumal <ssubhav19@gmail.com>
2019-02-22 17:11:17 +0530

    Generalized traffic scenario 07 for other dependent locations except traffic lights
    Traffic light generalization - in progress
    Right now setting up all traffic lights to green state -  will fix once TrafficAPI is fixed
    
    It needs to be tested with PR#39 changes as get_intersection method is used to find intersection

--- srunner/scenarios/opposite_vehicle_taking_priority.py
+++ srunner/scenarios/opposite_vehicle_taking_priority.py
@@ -20,7 +20,7 @@ import carla
 from srunner.scenariomanager.atomic_scenario_behavior import *
 from srunner.scenariomanager.atomic_scenario_criteria import *
 from srunner.scenarios.basic_scenario import *
-
+from srunner.scenarios.basic_scenario import get_intersection
 
 RUNNING_RED_LIGHT_SCENARIOS = [
     "OppositeVehicleRunningRedLight"
@@ -45,18 +45,15 @@ class OppositeVehicleRunningRedLight(BasicScenario):
     _ego_avg_velocity_expected = 4       # Average expected velocity [m/s]
     _ego_expected_driven_distance = 88   # Expected driven distance [m]
     _ego_distance_to_traffic_light = 15  # Trigger distance to traffic light [m]
-    _ego_end_position = carla.Location(x=-3, y=-90, z=0)   # End position
-    _ego_distance_to_end_position = 5    # Allowed distance to end position [m]
-    _intersection_location = carla.Location(x=-3, y=-150, z=0)
 
     # other vehicle
     _other_actor_target_velocity = 15      # Target velocity of other vehicle
     _other_actor_max_brake = 1.0           # Maximum brake of other vehicle
     _other_actor_distance = 30             # Distance the other vehicle should drive
 
-    _traffic_light_location = carla.Location(x=-11.5, y=-125.0, z=0.15)
     _traffic_light = None
-    _location_of_collision = carla.Location(x=0, y=-135, z=1)
+    _start_distance = 20
+    _end_distance = 80
 
     def __init__(self, world, ego_vehicle, other_actors, town, randomize=False, debug_mode=False):
         """
@@ -64,9 +61,7 @@ class OppositeVehicleRunningRedLight(BasicScenario):
         and instantiate scenario manager
         """
 
-        for actor in world.get_actors().filter('traffic.traffic_light'):
-            if actor.get_location().distance(self._traffic_light_location) < 1.0:
-                self._traffic_light = actor
+        self._traffic_light = world.get_actors().filter('traffic.traffic_light')
 
         if self._traffic_light is None:
             print("No traffic light for the given location found")
@@ -92,23 +87,21 @@ class OppositeVehicleRunningRedLight(BasicScenario):
         """
 
         # start condition
-        startcondition = InTriggerDistanceToLocation(
-            self.ego_vehicle,
-            self._intersection_location,
-            self._ego_distance_to_traffic_light,
-            name="Waiting for start position")
+        location, _ = get_location_in_distance(self.ego_vehicle, self._start_distance)
+        startcondition = InTriggerDistanceToLocation(self.ego_vehicle, location, 2.0)
 
-        # wait until traffic light for ego vehicle is green
-        wait_for_green = WaitForTrafficLightState(self._traffic_light, "Green")
+        for actor in self._traffic_light:
+            actor.set_state(carla.TrafficLightState.Green)
 
         sync_arrival_parallel = py_trees.composites.Parallel(
             "Synchronize arrival times",
             policy=py_trees.common.ParallelPolicy.SUCCESS_ON_ONE)
-        sync_arrival = SyncArrival(
-            self.other_actors[0], self.ego_vehicle, self._location_of_collision)
-        sync_arrival_stop = InTriggerDistanceToVehicle(self.other_actors[0],
-                                                       self.ego_vehicle,
-                                                       15)
+
+        target_location = get_intersection(self.other_actors[0], self.ego_vehicle)
+
+        sync_arrival = SyncArrival(self.other_actors[0], self.ego_vehicle, target_location)
+        sync_arrival_stop = InTriggerDistanceToVehicle(self.ego_vehicle, self.other_actors[0], 10)
+
         sync_arrival_parallel.add_child(sync_arrival)
         sync_arrival_parallel.add_child(sync_arrival_stop)
 
@@ -126,16 +119,11 @@ class OppositeVehicleRunningRedLight(BasicScenario):
         keep_velocity_for_distance.add_child(keep_velocity_distance)
 
         # finally wait that ego vehicle reached target position
-        wait = InTriggerDistanceToLocation(
-            self.ego_vehicle,
-            self._ego_end_position,
-            self._ego_distance_to_end_position,
-            name="Waiting for end position")
+        wait = DriveDistance(self.ego_vehicle, self._end_distance)
 
         # Build behavior tree
         sequence = py_trees.composites.Sequence("Sequence Behavior")
         sequence.add_child(startcondition)
-        sequence.add_child(wait_for_green)
         sequence.add_child(sync_arrival_parallel)
         sequence.add_child(keep_velocity_for_distance)
         sequence.add_child(wait)
