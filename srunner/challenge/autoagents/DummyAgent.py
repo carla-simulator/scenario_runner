57e91a4927aa6832db75ab9ced3e0e40d1a1bcdc
Felipe Codevilla <felipe.alcm@gmail.com>
2019-02-25 18:10:56 -0500

    final changes on the speedometer sampling rate

--- srunner/challenge/autoagents/DummyAgent.py
+++ srunner/challenge/autoagents/DummyAgent.py
@@ -28,7 +28,6 @@ class DummyAgent(AutonomousAgent):
         """
         sensors = [{'type': 'sensor.camera.rgb', 'x':0.7, 'y':0.0, 'z':1.60, 'roll':0.0, 'pitch':0.0, 'yaw':0.0,
                     'width':800, 'height': 600, 'fov':100, 'id': 'Center'},
-
                    {'type': 'sensor.camera.rgb', 'x':0.7, 'y':-0.4, 'z': 1.60, 'roll': 0.0, 'pitch': 0.0,
                     'yaw': -45.0, 'width': 800, 'height': 600, 'fov': 100, 'id': 'Left'},
                    {'type': 'sensor.camera.rgb', 'x':0.7, 'y':0.4, 'z':1.60, 'roll':0.0, 'pitch':0.0, 'yaw':45.0,
