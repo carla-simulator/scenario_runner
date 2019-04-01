d73799a15c9d0d21e9674eb238bd736c064a9938
Subhashini Perumal <ssubhav19@gmail.com>
2019-04-01 13:04:08 +0530

    Added avoid_collision flag.

--- srunner/scenarios/signalized_junction_left_turn.py
+++ srunner/scenarios/signalized_junction_left_turn.py
@@ -17,8 +17,6 @@ from srunner.scenariomanager.carla_data_provider import CarlaDataProvider
 from srunner.scenarios.basic_scenario import *
 from srunner.scenarios.scenario_helper import *
 
-from queue import Queue
-
 TURN_LEFT_SIGNALIZED_JUNCTION_SCENARIOS = [
     "SignalizedJunctionLeftTurn"
 ]
@@ -36,17 +34,16 @@ class SignalizedJunctionLeftTurn(BasicScenario):
         """
         Setup all relevant parameters and create scenario
         """
-
-        self._world = world
         self.category = "SignalizedJunctionLeftTurn"
-        self.timeout = 60  # Timeout of scenario in seconds
+        self.timeout = 80  # Timeout of scenario in seconds
         self._target_vel = 35
-        self._drive_distance = 200
+        self._brake_value = 0.5
+        self._drive_distance = 50
+        self._ego_distance = 20
+        self._dist_to_intersection = 12
+        self._start_distance = 3
         self._traffic_light = None
         self._other_actor_transform = None
-        self._sink_location = None
-        self._blackboard_queue_name = 'SignalizedJunctionLeftTurn/actor_flow_queue'
-        self._queue = Blackboard().set(self._blackboard_queue_name, Queue())
 
         super(SignalizedJunctionLeftTurn, self).__init__("SignalizedJunctionLeftTurn",
                                                          ego_vehicle,
@@ -77,7 +74,7 @@ class SignalizedJunctionLeftTurn(BasicScenario):
         first_vehicle_transform = carla.Transform(
             carla.Location(config.other_actors[0].transform.location.x,
                            config.other_actors[0].transform.location.y,
-                           config.other_actors[0].transform.location.z - 5),
+                           config.other_actors[0].transform.location.z),
             config.other_actors[0].transform.rotation)
         first_vehicle = CarlaActorPool.request_new_actor(config.other_actors[0].model, first_vehicle_transform)
         self.other_actors.append(first_vehicle)
@@ -89,43 +86,53 @@ class SignalizedJunctionLeftTurn(BasicScenario):
         coming straight crossing from an opposite direction,
         After 80 seconds, a timeout stops the scenario.
         """
-
-        root = py_trees.composites.Parallel(policy=py_trees.common.ParallelPolicy.SUCCESS_ON_ONE)
-
-        actor_source = ActorSource(
-            self._world, ['vehicle.audi.tt', 'vehicle.tesla.model3', 'vehicle.nissan.micra'],
-            self._other_actor_transform, 20, self._blackboard_queue_name)
-
+        waypoint = self.ego_vehicle.get_world().get_map().get_waypoint(self.ego_vehicle.get_location())
+        wp_choice = waypoint.next(2)
+        while not wp_choice[0].is_intersection:
+            waypoint = wp_choice[0]
+            wp_choice = waypoint.next(2)
+        target_wp = choose_at_junction(waypoint, waypoint.next(2), direction=-1)
+        start_other_trigger = InTriggerDistanceToLocation(
+            self.ego_vehicle,
+            target_wp.transform.location, self._dist_to_intersection)
         # Selecting straight path at intersection
         target_waypoint = generate_target_waypoint(
             CarlaDataProvider.get_map().get_waypoint(self.other_actors[0].get_location()), 0)
-
         # Generating waypoint list till next intersection
         plan = []
-        wp_choice = target_waypoint.next(1.0)
+        wp_choice = target_waypoint.next(5.0)
         while len(wp_choice) == 1:
             target_waypoint = wp_choice[0]
             plan.append((target_waypoint, RoadOption.LANEFOLLOW))
-            wp_choice = target_waypoint.next(1.0)
+            wp_choice = target_waypoint.next(5.0)
+        location, _ = get_location_in_distance(self.ego_vehicle, self._start_distance)
+        start_condition = InTriggerDistanceToLocation(self.ego_vehicle, location, 2)
+        move_other_actor = WaypointFollower(self.other_actors[0], self._target_vel, plan=plan, avoid_collision=True)
+        move_actor = WaypointFollower(self.other_actors[0], self._target_vel, plan=plan, avoid_collision=True)
+        drive_actor = DriveDistance(self.other_actors[0], self._drive_distance)
+        stop_other = StopVehicle(self.other_actors[0], self._brake_value)
+        end_condition = DriveDistance(self.ego_vehicle, self._ego_distance)
+        move_actor_parallel = py_trees.composites.Parallel(
+            policy=py_trees.common.ParallelPolicy.SUCCESS_ON_ONE)
+        move_actor_parallel.add_child(move_actor)
+        move_actor_parallel.add_child(drive_actor)
+        move_other_actor_parallel = py_trees.composites.Parallel(
+            policy=py_trees.common.ParallelPolicy.SUCCESS_ON_ONE)
+        move_other_actor_parallel.add_child(move_other_actor)
+        move_other_actor_parallel.add_child(InTriggerDistanceToNextIntersection(self.other_actors[0], 10))
 
-        waypoint_follower = WaypointFollower(
-            self.other_actors[0], self._target_vel, plan=plan, blackboard_queue_name=self._blackboard_queue_name, avoid_collision=False)
+        sequence = py_trees.composites.Sequence()
+        sequence.add_child(ActorTransformSetter(self.other_actors[0], self._other_actor_transform))
+        sequence.add_child(start_condition)
+        sequence.add_child(move_other_actor_parallel)
+        sequence.add_child(stop_other)
+        sequence.add_child(start_other_trigger)
+        sequence.add_child(move_actor_parallel)
+        sequence.add_child(stop_other)
+        sequence.add_child(end_condition)
+        sequence.add_child(ActorDestroy(self.other_actors[0]))
 
-        sink_waypoint = target_waypoint.next(1)[0]
-        while not sink_waypoint.is_intersection:
-            sink_waypoint = sink_waypoint.next(1)[0]
-        self._sink_location = sink_waypoint.transform.location
-
-        actor_sink = ActorSink(self._world, self._sink_location, 10)
-
-        drive_distance = DriveDistance(self.ego_vehicle, self._drive_distance)
-
-        root.add_child(actor_source)
-        root.add_child(waypoint_follower)
-        root.add_child(actor_sink)
-        root.add_child(drive_distance)
-
-        return root
+        return sequence
 
     def _create_test_criteria(self):
         """
