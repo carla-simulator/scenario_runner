abcc5c3c6db26016ed05db396337e6375ac5f3aa
German Ros <germanros1987@gmail.com>
2019-04-08 20:13:22 -0700

    minor fixes

--- srunner/challenge/make_docker.sh
+++ srunner/challenge/make_docker.sh
@@ -14,9 +14,14 @@ fi
 # copy required resources from the host
 cp -fr ${CARLA_ROOT}/PythonAPI  .
 
+mkdir .tmp
+git clone -b carla_challenge --single-branch https://github.com/carla-simulator/scenario_runner.git .tmp
+
 
 # build docker image
 docker build --force-rm --build-arg HTTP_PROXY=${HTTP_PROXY} \
              --build-arg HTTPS_PROXY=${HTTPS_PROXY} \
              --build-arg http_proxy=${http_proxy} \
              -t carla-challenge-master -f ${ROOT_SCENARIO_RUNNER}/srunner/challenge/Dockerfile.master .
+
+rm -fr .tmp
