a1791ccc38fb61921c59dc15b15af901413517a8
German Ros <germanros1987@gmail.com>
2019-04-08 20:47:17 -0700

    minor fix

--- team_code/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-TEAM_AGENT=/workspace/team_code/YOUR_AGENT.py
-TEAM_CONFIG=/workspace/team_code/YOUR_CONFIG_FILE
-
-echo "export TEAM_AGENT=${TEAM_AGENT}" >> ~/.bashrc
-echo "export TEAM_CONFIG=${TEAM_CONFIG}" >> ~/.bashrc
