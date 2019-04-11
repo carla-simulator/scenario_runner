1cf89e38b01441b0c96171231fdbb4866e2e6932
Felipe Codevilla <felipe.alcm@gmail.com>
2019-04-11 11:14:37 -0400

    finish rebase

--- srunner/scenarios/object_crash_vehicle.py
+++ srunner/scenarios/object_crash_vehicle.py
@@ -228,10 +228,8 @@ class DynamicObjectCrossing(BasicScenario):
         """
         Custom initialization
         """
-
         # cyclist transform
         _start_distance = 10
-        counter = 10
         # We start by getting and waypoint in the closest sidewalk.
         waypoint = self._reference_waypoint
         while True:
@@ -246,10 +244,7 @@ class DynamicObjectCrossing(BasicScenario):
                 break
 
         while True:  # We keep trying to spawn avoiding props
-            counter -=1
-            if counter == 0:
-                self._initialization_status =False
-                return
+
             try:
                 self.transform, orientation_yaw = self._calculate_base_transform(_start_distance, waypoint)
                 first_vehicle = self._spawn_adversary(self.transform, orientation_yaw)
