af45902732473cf4c0fbb47a304e074cff60d6b6
German Ros <germanros1987@gmail.com>
2019-04-12 13:43:12 -0700

    removing ts08 due to problems

--- srunner/challenge/run_evaluator.sh
+++ srunner/challenge/run_evaluator.sh
@@ -4,6 +4,6 @@
 # !Make sure you set $CHALLENGE_TIME_AVAILABLE (e.g. 10000)
 
 python ${ROOT_SCENARIO_RUNNER}/srunner/challenge/challenge_evaluator_routes.py \
---scenarios=${ROOT_SCENARIO_RUNNER}/srunner/challenge/all_towns_traffic_scenarios1_3_4_8.json \
+--scenarios=${ROOT_SCENARIO_RUNNER}/srunner/challenge/all_towns_traffic_scenarios1_3_4.json \
 --debug=0 \
 --agent=${ROOT_SCENARIO_RUNNER}/srunner/challenge/autoagents/HumanAgent.py
