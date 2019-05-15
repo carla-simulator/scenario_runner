d68280a6200854c87952b247057a321d3ba783b5
German Ros <germanros1987@gmail.com>
2019-05-15 16:16:41 -0700

    Fix: Pedestrian is now mortal and pedestrian impacts are not double counted

--- srunner/scenariomanager/carla_data_provider.py
+++ srunner/scenariomanager/carla_data_provider.py
@@ -434,6 +434,11 @@ class CarlaActorPool(object):
 
         # Get vehicle by model
         blueprint = random.choice(blueprint_library.filter(model))
+
+        # is it a pedestrian? -> make it mortal
+        if blueprint.has_attribute('is_invincible'):
+            blueprint.set_attribute('is_invincible', 'false')
+
         if hero:
             blueprint.set_attribute('role_name', 'hero')
         elif autopilot:
@@ -482,6 +487,10 @@ class CarlaActorPool(object):
             hero_actor = None
         batch = []
         for _ in range(amount):
+            # is it a pedestrian? -> make it mortal
+            if blueprint.has_attribute('is_invincible'):
+                blueprint.set_attribute('is_invincible', 'false')
+
             # Get vehicle by model
             blueprint = random.choice(blueprint_library.filter(model))
             if hero:
