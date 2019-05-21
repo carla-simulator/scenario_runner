2543673f1527095bad473867b176b07c774ebc2a
damascenodiego <damascenodiego@usp.br>
2019-05-21 12:06:12 +0100

    append path to carla .egg files

--- weather_control.py
+++ weather_control.py
@@ -1,6 +1,14 @@
 from tkinter import *
 
 
+try:
+    sys.path.append(glob.glob('../carla/dist/carla-*%d.%d-%s.egg' % (
+        sys.version_info.major,
+        sys.version_info.minor,
+        'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
+except IndexError:
+    pass
+
 import carla
 import argparse
 
