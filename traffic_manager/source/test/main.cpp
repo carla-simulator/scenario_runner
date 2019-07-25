bcb157fd14852c2f35a0fe315b2c04ed6393d8b9
Praveen Kumar <pravinblaze@hotmail.com>
2019-07-25 19:18:08 +0530

    updating steer pid values

--- traffic_manager/source/test/main.cpp
+++ traffic_manager/source/test/main.cpp
@@ -82,7 +82,7 @@ void test_pipeline (
     float target_velocity = 7.0;
     traffic_manager::MotionPlannerCallable actor_pid_callable(
         target_velocity, &tl_queue, &pid_queue, &shared_data,
-        {0.1f, 0.15f, 0.01f}, {5.0f, 0.1f, 1.0f});
+        {0.1f, 0.15f, 0.01f}, {10.0f, 0.05f, 0.1f});
     traffic_manager::PipelineStage actor_pid_stage(8, actor_pid_callable);
     actor_pid_stage.start();
 
