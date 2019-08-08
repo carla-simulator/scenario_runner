e6cd3bfb775b5767144abf33d4e8f114045e0b0f
Fabian Oboril <fabian.oboril@intel.com>
2019-08-08 17:09:51 +0200

    Fixed missing rolename for ego vehicle in challenge_evaluator_routes
    Change-Id: Iaca90aa71e903faa85999d6979323db85d091116

--- srunner/challenge/challenge_evaluator_routes.py
+++ srunner/challenge/challenge_evaluator_routes.py
@@ -308,7 +308,10 @@ class ChallengeEvaluator(object):
         # Otherwise spawn ego vehicle
         if self.ego_vehicle is None:
             # TODO: the model is now hardcoded but that can change in a future.
-            self.ego_vehicle = CarlaActorPool.request_new_actor('vehicle.lincoln.mkz2017', start_transform, hero=True)
+            self.ego_vehicle = CarlaActorPool.request_new_actor('vehicle.lincoln.mkz2017',
+                                                                start_transform,
+                                                                rolename='hero',
+                                                                hero=True)
         else:
             self.ego_vehicle.set_transform(start_transform)
 
