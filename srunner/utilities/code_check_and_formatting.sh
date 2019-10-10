32ee9adac9b3b963f45673a16b595001f19dfc04
German Ros <germanros1987@gmail.com>
2019-10-10 12:56:35 -0700

    Fixing pylint attempt 3

--- srunner/utilities/code_check_and_formatting.sh
+++ srunner/utilities/code_check_and_formatting.sh
@@ -10,10 +10,10 @@ autopep8 srunner/tools/*.py --in-place --max-line-length=120
 autopep8 srunner/scenarioconfigs/*.py --in-place --max-line-length=120
 
 
-pylint --rcfile=.pylintrc --disable=I --extension-pkg-whitelist=numpy srunner/scenariomanager/
-pylint --rcfile=.pylintrc --extension-pkg-whitelist=numpy srunner/scenarios/
-pylint --rcfile=.pylintrc --extension-pkg-whitelist=numpy srunner/challenge/autoagents/
-pylint --rcfile=.pylintrc --extension-pkg-whitelist=numpy srunner/challenge/challenge_statistics_manager.py
-pylint --rcfile=.pylintrc --extension-pkg-whitelist=numpy srunner/tools/
-pylint --rcfile=.pylintrc --extension-pkg-whitelist=numpy srunner/scenarioconfigs/
-pylint --rcfile=.pylintrc --extension-pkg-whitelist=numpy scenario_runner.py
+pylint --rcfile=.pylintrc --disable=I srunner/scenariomanager/
+pylint --rcfile=.pylintrc srunner/scenarios/
+pylint --rcfile=.pylintrc srunner/challenge/autoagents/
+pylint --rcfile=.pylintrc srunner/challenge/challenge_statistics_manager.py
+pylint --rcfile=.pylintrc srunner/tools/
+pylint --rcfile=.pylintrc srunner/scenarioconfigs/
+pylint --rcfile=.pylintrc scenario_runner.py
