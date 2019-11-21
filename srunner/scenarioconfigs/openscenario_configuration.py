f196d73d0230212cf1745beb0aa9d844db2f5fba
Antonia Alice Frey <Antonia.frey@outlook.com>
2019-11-21 10:53:57 +0100

    Updated _get_actor_transform() comment
    Note: - The OpenScenario specification allows multiple definitions. We use the _first_ one
                  - The OpenScenario specification allows different ways of specifying a position.
                    We currently support the specification with absolute world coordinates and the relative positions RelativeWorld,
                    RelativeObject and RelativeLane
                  - When using relative positions the relevant reference point (e.g. transform of another actor) should be defined before!

--- srunner/scenarioconfigs/openscenario_configuration.py
+++ srunner/scenarioconfigs/openscenario_configuration.py
@@ -205,7 +205,9 @@ class OpenScenarioConfiguration(ScenarioConfiguration):
 
         Note: - The OpenScenario specification allows multiple definitions. We use the _first_ one
               - The OpenScenario specification allows different ways of specifying a position.
-                We currently only support a specification with absolute world coordinates
+                We currently support the specification with absolute world coordinates and the relative positions RelativeWorld,
+                RelativeObject and RelativeLane
+              - When using relative positions the relevant reference point (e.g. transform of another actor) should be defined before!
         """
 
         actor_transform = carla.Transform()
