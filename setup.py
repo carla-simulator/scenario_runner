16f7f52e46a36ca34ad9f1e3f92cb7ab2aa4d335
felipecode <felipe.alcm@gmail.com>
2019-11-29 20:55:57 +0100

    updated pip to add dependencies

--- setup.py
+++ setup.py
@@ -5,15 +5,23 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='srunner',
-     version='0.15',
-     scripts=[],
+     version='0.4',
+     scripts=['scenario_runner.py', 'manual_control.py'],
      author="Carla Team",
-     author_email="carla.simulatort@gmail.com",
+     author_email="carla.simulator@gmail.com",
      description="The package in order to use scenarios for CARLA",
      long_description=long_description,
      long_description_content_type="text/markdown",
      url="https://github.com/carla-simulator/scenario_runner",
      packages=setuptools.find_packages(),
+     install_requires=[
+        'xmlschema',
+        'py_trees==0.8.3',
+        'six>=1.13.0',
+        'numpy>=1.16',
+         'networkx',
+         'shapely'
+     ],
      classifiers=[
          "Programming Language :: Python :: 3",
          "License :: OSI Approved :: MIT License",
