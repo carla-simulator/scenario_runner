6f263d41f56e811c0ae1d545a99f69bb0daa7ddc
glopezdiest <glopez@cvc.uab.es>
2020-04-08 09:00:41 +0200

    Removed more references to the challenge

--- srunner/autoagents/sensor_interface.py
+++ srunner/autoagents/sensor_interface.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+
+# This work is licensed under the terms of the MIT license.
+# For a copy, see <https://opensource.org/licenses/MIT>.
+
+
 import copy
 import logging
 import numpy as np
