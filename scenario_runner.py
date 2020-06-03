066cc8d3f9a6f913162bd3f73f49a2b5c0bf987c
glopezdiest <glopez@cvc.uab.es>
2020-06-03 09:36:13 +0200

    First Iter output changes

--- scenario_runner.py
+++ scenario_runner.py
@@ -234,6 +234,7 @@ class ScenarioRunner(object):
         Provide feedback about success/failure of a scenario
         """
 
+        # Create the filename
         current_time = str(datetime.now().strftime('%Y-%m-%d-%H-%M-%S'))
         junit_filename = None
         config_name = config.name
