eedb67c3a56716b0095fdce2667d0745bd06bae5
glopezdiest <glopez@cvc.uab.es>
2020-06-29 14:21:06 +0200

    Avoided importing everything

--- manual_control.py
+++ manual_control.py
@@ -55,12 +55,32 @@ from __future__ import print_function
 
 import carla
 
-from examples.manual_control import *
+from examples.manual_control import (World, HUD, KeyboardControl, CameraManager,
+                                     CollisionSensor, LaneInvasionSensor, GnssSensor, IMUSensor)
 
+import re
 import os
 import argparse
 import logging
 import time
+import pygame
+
+# ==============================================================================
+# -- Global functions ----------------------------------------------------------
+# ==============================================================================
+
+
+def find_weather_presets():
+    rgx = re.compile('.+?(?:(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])|$)')
+    name = lambda x: ' '.join(m.group(0) for m in rgx.finditer(x))
+    presets = [x for x in dir(carla.WeatherParameters) if re.match('[A-Z].+', x)]
+    return [(getattr(carla.WeatherParameters, x), name(x)) for x in presets]
+
+
+def get_actor_display_name(actor, truncate=250):
+    name = ' '.join(actor.type_id.replace('_', '.').title().split('.')[1:])
+    return (name[:truncate - 1] + u'\u2026') if len(name) > truncate else name
+
 
 # ==============================================================================
 # -- World ---------------------------------------------------------------------
