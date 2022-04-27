1d6138b66bf6284b6b3a6e363a52ebf4a4861691
Tay Yim <yan_sy@126.com>
2022-04-27 22:41:34 +0800

    New Parking Exit scenario

--- srunner/scenariomanager/scenarioatomics/atomic_criteria.py
+++ srunner/scenariomanager/scenarioatomics/atomic_criteria.py
@@ -1003,6 +1003,7 @@ class OutsideRouteLanesTest(Criterion):
         self._last_lane_id = None
         self._total_distance = 0
         self._wrong_distance = 0
+        self._active = True
 
     def update(self):
         """
@@ -1020,11 +1021,16 @@ class OutsideRouteLanesTest(Criterion):
         if location is None:
             return new_status
 
-        # Check if outside route lanes
+        # Deactivate/Activate checking by blackboard message
+        active = py_trees.blackboard.Blackboard().get('AC_SwitchOutsideRouteLanesTest')
+        if active is not None:
+            self._active = active
+            py_trees.blackboard.Blackboard().set("AC_SwitchOutsideRouteLanesTest", None, overwrite=True)
+
         self._is_outside_driving_lanes(location)
         self._is_at_wrong_lane(location)
 
-        if self._outside_lane_active or self._wrong_lane_active:
+        if self._active and (self._outside_lane_active or self._wrong_lane_active):
             self.test_status = "FAILURE"
 
         # Get the traveled distance
@@ -1044,10 +1050,11 @@ class OutsideRouteLanesTest(Criterion):
                 self._total_distance += new_dist
 
                 # And to the wrong one if outside route lanes
-                if self._outside_lane_active or self._wrong_lane_active:
+                if self._active and (self._outside_lane_active or self._wrong_lane_active):
                     self._wrong_distance += new_dist
 
                 self._current_index = index
+
 
         self.logger.debug("%s.update()[%s->%s]" % (self.__class__.__name__, self.status, new_status))
         return new_status
---
+++ srunner/scenarios/parking_exit.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2018-2020 Intel Corporation
+#
+# This work is licensed under the terms of the MIT license.
+# For a copy, see <https://opensource.org/licenses/MIT>.
+
+"""
+Scenario in which the ego is parked between two vehicles and has to maneuver to start the route.
+"""
+
+from __future__ import print_function
+
+import py_trees
+import carla
+
+
+from srunner.scenariomanager.carla_data_provider import CarlaDataProvider
+from srunner.scenariomanager.scenarioatomics.atomic_behaviors import SwitchOutsideRouteLanesTest, ActorTransformSetter, ActorDestroy
+from srunner.scenariomanager.scenarioatomics.atomic_criteria import CollisionTest
+from srunner.scenariomanager.scenarioatomics.atomic_trigger_conditions import DriveDistance
+from srunner.scenarios.basic_scenario import BasicScenario
+
+
+def convert_dict_to_location(actor_dict):
+    """
+    Convert a JSON string to a Carla.Location
+    """
+    location = carla.Location(
+        x=float(actor_dict['x']),
+        y=float(actor_dict['y']),
+        z=float(actor_dict['z'])
+    )
+    return location
+
+
+class ParkingExit(BasicScenario):
+    """
+    This class holds everything required for a scenario in which the ego would be teleported to the parking lane.
+    Once the scenario is triggered, the OutsideRouteLanesTest will be deactivated since the ego is out of the driving lane.
+    Then blocking vehicles will be generated in front of and behind the parking point.
+    The ego need to exit from the parking lane and then merge into the driving lane.
+    After the ego is {end_distance} meters away from the parking point, the OutsideRouteLanesTest will be activated and the scenario ends.
+
+    Note 1: For route mode, this shall be the first scenario of the route. The trigger point shall be the first point of the route waypoints.
+
+    Note 2: Make sure there are enough space for spawning blocking vehicles.
+    """
+
+    def __init__(self, world, ego_vehicles, config, debug_mode=False, criteria_enable=True,
+                 timeout=180):
+        """
+        Setup all relevant parameters and create scenario
+        and instantiate scenario manager
+        """
+        self._world = world
+        self._map = CarlaDataProvider.get_map()
+        self.timeout = timeout
+
+        if 'front_vehicle_distance' in config.other_parameters:
+            self._front_vehicle_distance = float(
+                config.other_parameters['front_vehicle_distance']['value'])
+        else:
+            self._front_vehicle_distance = 20  # m
+
+        if 'behind_vehicle_distance' in config.other_parameters:
+            self._behind_vehicle_distance = float(
+                config.other_parameters['behind_vehicle_distance']['value'])
+        else:
+            self._behind_vehicle_distance = 5  # m
+
+        self._end_distance = self._front_vehicle_distance + 15
+
+        if 'parking_lane_side' in config.other_parameters:
+            self._parking_lane_side = config.other_parameters['parking_lane_side']['value']
+        else:
+            self._parking_lane_side = "right"
+
+        # Get parking_waypoint based on trigger_point
+        self._trigger_location = config.trigger_points[0].location
+        self._reference_waypoint = self._map.get_waypoint(self._trigger_location)
+        if self._parking_lane_side == "left":
+            self._parking_waypoint = self._reference_waypoint.get_left_lane()
+        else:
+            self._parking_waypoint = self._reference_waypoint.get_right_lane()
+
+        if self._parking_waypoint is None:
+            raise Exception(
+                "Couldn't find parking point on the {} side".format(self._parking_lane_side))
+
+        super(ParkingExit, self).__init__("ParkingExit",
+                                          ego_vehicles,
+                                          config,
+                                          world,
+                                          debug_mode,
+                                          criteria_enable=criteria_enable)
+
+    def _initialize_actors(self, config):
+        """
+        Custom initialization
+        """
+        # Put blocking vehicles
+        front_points = self._parking_waypoint.next(
+            self._front_vehicle_distance)
+        if front_points:
+            actor_front = CarlaDataProvider.request_new_actor(
+                'vehicle.*', front_points[0].transform, rolename='scenario', attribute_filter={'base_type': 'car'})
+            if actor_front is None:
+                raise Exception(
+                    "Couldn't spawn the vehicle in front of the parking point")
+            self.other_actors.append(actor_front)
+        else:
+            raise Exception(
+                "Couldn't find viable position for the vehicle in front of the parking point")
+
+        behind_points = self._parking_waypoint.previous(
+            self._behind_vehicle_distance)
+        if behind_points:
+            actor_behind = CarlaDataProvider.request_new_actor(
+                'vehicle.*', behind_points[0].transform, rolename='scenario', attribute_filter={'base_type': 'car'})
+            if actor_behind is None:
+                raise Exception(
+                    "Couldn't spawn the vehicle behind the parking point")
+            self.other_actors.append(actor_behind)
+        else:
+            raise Exception(
+                "Couldn't find viable position for the vehicle behind the parking point")
+
+    def _create_behavior(self):
+        """
+        Deactivate OutsideRouteLanesTest, then move ego to the parking point, generate blocking vehicles in front of and behind the ego.
+        After ego drives away, actviate OutsideRouteLanesTest, end scenario.
+        """
+
+        sequence = py_trees.composites.Sequence()
+
+        # Deactivate OutsideRouteLanesTest
+        sequence.add_child(SwitchOutsideRouteLanesTest(False))
+
+        # Teleport ego to the parking point
+        sequence.add_child(ActorTransformSetter(
+            self.ego_vehicles[0], self._parking_waypoint.transform))
+
+        root = py_trees.composites.Parallel(
+            policy=py_trees.common.ParallelPolicy.SUCCESS_ON_ONE)
+
+        end_condition = py_trees.composites.Sequence()
+        end_condition.add_child(DriveDistance(
+            self.ego_vehicles[0], self._end_distance))
+        root.add_child(end_condition)
+        sequence.add_child(root)
+
+        sequence.add_child(SwitchOutsideRouteLanesTest(True))
+
+        for actor in self.other_actors:
+            sequence.add_child(ActorDestroy(actor))
+
+        return sequence
+
+    def _create_test_criteria(self):
+        """
+        A list of all test criteria will be created that is later used
+        in parallel behavior tree.
+        """
+        if self.route_mode:
+            return[]
+
+        return [CollisionTest(self.ego_vehicles[0])]
+
+    def __del__(self):
+        """
+        Remove all actors and traffic lights upon deletion
+        """
+        self.remove_all_actors()
