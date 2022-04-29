06ec676650397e9e1630076dc852f698f99669c5
Songyang Yan <lapalacayim@gmail.com>
2022-04-29 15:23:19 +0800

    temp save

--- srunner/scenarios/yield_to_emergency_vehicle.py
+++ srunner/scenarios/yield_to_emergency_vehicle.py
@@ -16,11 +16,11 @@ import carla
 
 
 from srunner.scenariomanager.carla_data_provider import CarlaDataProvider
-from srunner.scenariomanager.scenarioatomics.atomic_behaviors import ActorTransformSetter, ActorDestroy, WaypointFollower
+from srunner.scenariomanager.scenarioatomics.atomic_behaviors import ActorTransformSetter, ActorDestroy, WaypointFollower, SwitchOutsideRouteLanesTest
 from srunner.scenariomanager.scenarioatomics.atomic_criteria import CollisionTest
-from srunner.scenariomanager.scenarioatomics.atomic_trigger_conditions import InTriggerDistanceToLocation
+from srunner.scenariomanager.scenarioatomics.atomic_trigger_conditions import InTriggerDistanceToLocation, DriveDistance
 from srunner.scenarios.basic_scenario import BasicScenario
-from srunner.tools.background_manager import RemoveLane
+from srunner.tools.background_manager import RemoveLane, SwitchRouteSources, SwitchLane
 
 import operator
 import random
@@ -54,21 +54,25 @@ class YieldToEmergencyVehicle(BasicScenario):
         self._world = world
         self._map = CarlaDataProvider.get_map()
         self.timeout = timeout
-        # self._actor_types = ["vehicle.carlamotors.firetruck", "vehicle.ford.ambulance", "vehicle.dodge.charger_police", "vehicle.dodge.charger_police_2020"]
-        self._actor_types = ["vehicle.carlamotors.firetruck", "vehicle.ford.ambulance"]
+        self._actor_types = ["vehicle.carlamotors.firetruck", "vehicle.ford.ambulance", "vehicle.dodge.charger_police", "vehicle.dodge.charger_police_2020"]
+        self._ev_drive_distance = 150
 
         if 'emergency_vehicle_distance' in config.other_parameters:
             self._emergency_vehicle_distance = float(
                 config.other_parameters['emergency_vehicle_distance']['value'])
         else:
-            self._emergency_vehicle_distance = 30  # m
+            self._emergency_vehicle_distance = 10  # m
+
+        self._trigger_location = config.trigger_points[0].location
+        self._reference_waypoint = self._map.get_waypoint(
+            self._trigger_location)
 
         super(YieldToEmergencyVehicle, self).__init__("YieldToEmergencyVehicle",
-                                          ego_vehicles,
-                                          config,
-                                          world,
-                                          debug_mode,
-                                          criteria_enable=criteria_enable)
+                                                      ego_vehicles,
+                                                      config,
+                                                      world,
+                                                      debug_mode,
+                                                      criteria_enable=criteria_enable)
 
     def _initialize_actors(self, config):
         """
@@ -76,22 +80,15 @@ class YieldToEmergencyVehicle(BasicScenario):
         """
         # Spawn emergency vehicle
 
-        spawn_points = self._map.get_waypoint(
-                self.ego_vehicles[0].get_location()).previous(self._emergency_vehicle_distance)
+        spawn_transform = self._reference_waypoint.transform
+        spawn_transform.location.z -= 500
 
-        if spawn_points:
-            actor = CarlaDataProvider.request_new_actor(
-                random.choice(self._actor_types), spawn_points[0].transform, rolename='scenario', attribute_filter={'base_type': 'car'})
-            print(spawn_points[0].transform)
-            print(actor.get_location())
-            if actor is None:
-                raise Exception(
-                    "Couldn't spawn the emergency vehicle")
-            self.other_actors.append(actor)
-        else:
+        actor = CarlaDataProvider.request_new_actor(
+            random.choice(self._actor_types), spawn_transform, rolename='scenario')
+        if actor is None:
             raise Exception(
-                "Couldn't find viable position for the emergency vehicle")
-
+                "Couldn't spawn the emergency vehicle")
+        self.other_actors.append(actor)
 
     def _create_behavior(self):
         """
@@ -100,41 +97,51 @@ class YieldToEmergencyVehicle(BasicScenario):
 
         sequence = py_trees.composites.Sequence()
 
-
-
-
-        
-
         # # Deactivate OutsideRouteLanesTest
         # sequence.add_child(SwitchOutsideRouteLanesTest(False))
 
-        # # Teleport ego to the parking point
-        # sequence.add_child(ActorTransformSetter(
-        #     self.ego_vehicles[0], self._parking_waypoint.transform))
-
         root = py_trees.composites.Parallel(
-            policy=py_trees.common.ParallelPolicy.SUCCESS_ON_ONE)
+            policy=py_trees.common.ParallelPolicy.SUCCESS_ON_ALL)
 
-        root.add_child(RemoveLane(self._map.get_waypoint(self.ego_vehicles[0].get_location()).lane_id))
+        # sequence.add_child(RemoveLane(self._reference_waypoint.lane_id))
+        sequence.add_child(SwitchLane(self._reference_waypoint.lane_id, False))
+
+        # Teleport EV behind the ego
+        ev_points = self._map.get_waypoint(self.ego_vehicles[0].get_location()).previous(
+            self._emergency_vehicle_distance)
+        if ev_points:
+            ev_start_transform = ev_points[0].transform
+        else:
+            raise Exception(
+                "Couldn't find viable position for the emergency vehicle")
+        sequence.add_child(ActorTransformSetter(
+            self.other_actors[0], ev_start_transform))
 
 
-        # end_condition = py_trees.composites.Sequence()
-        # end_condition.add_child(InTriggerDistanceToLocation(
-        #     self.ego_vehicles[0], self._parking_waypoint.transform.location, self._end_distance, operator.gt, name="EndTrigger"))
-        # root.add_child(end_condition)
-        sequence.add_child(root)
+        sequence.add_child(SwitchOutsideRouteLanesTest(False))
 
-        ev = py_trees.composites.Parallel(
-            policy=py_trees.common.ParallelPolicy.SUCCESS_ON_ONE)
+        ev_end_condition = py_trees.composites.Parallel("Waiting for emergency vehicle driving for a certein distance",
+                                                    policy=py_trees.common.ParallelPolicy.SUCCESS_ON_ONE)
 
-        ev.add_child(WaypointFollower(self.other_actors[0], 30, avoid_collision=True))
-        sequence.add_child(ev)
+        ev_end_condition.add_child(DriveDistance(
+            self.other_actors[0], self._ev_drive_distance))
+        ev_end_condition.add_child(WaypointFollower(
+            self.other_actors[0], 80, avoid_collision=True))
+        sequence.add_child(ev_end_condition)
 
+        sequence.add_child(ActorDestroy(self.other_actors[0]))
 
-        # sequence.add_child(SwitchOutsideRouteLanesTest(True))
+        end_condition = py_trees.composites.Parallel("Waiting for ego driving for a certein distance",
+                                                    policy=py_trees.common.ParallelPolicy.SUCCESS_ON_ONE)
+        end_condition.add_child(DriveDistance(
+            self.ego_vehicles[0], 30))
 
-        # for actor in self.other_actors:
-        #     sequence.add_child(ActorDestroy(actor))
+        sequence.add_child(end_condition)
+
+        sequence.add_child(SwitchOutsideRouteLanesTest(True))
+
+        # sequence.add_child(SwitchRouteSources(True))
+        sequence.add_child(SwitchLane(self._reference_waypoint.lane_id, True))
 
         return sequence
 
