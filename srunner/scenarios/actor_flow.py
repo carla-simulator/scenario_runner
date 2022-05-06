8b223b6d4061006f12c7fcc3029455d2d95ee9e2
Tay Yim <yan_sy@126.com>
2022-05-06 22:48:12 +0800

    Changed BA 'RemoveLane' for 'SwitchLane'

--- srunner/scenarios/actor_flow.py
+++ srunner/scenarios/actor_flow.py
@@ -25,7 +25,7 @@ from srunner.scenarios.basic_scenario import BasicScenario
 
 from srunner.tools.background_manager import (SwitchRouteSources,
                                               ExtentExitRoadSpace,
-                                              RemoveLane,
+                                              SwitchLane,
                                               RemoveJunctionEntry,
                                               ClearJunction)
 from srunner.tools.scenario_helper import get_same_dir_lanes
@@ -307,8 +307,11 @@ class HighwayExit(BasicScenario):
         sequence = py_trees.composites.Sequence()
 
         if self.route_mode:
-            sequence.add_child(RemoveLane(source_wp.lane_id))
+            sequence.add_child(SwitchLane(source_wp.lane_id, False))
         sequence.add_child(root)
+
+        if self.route_mode:
+            sequence.add_child(SwitchLane(source_wp.lane_id, True))
 
         return sequence
 
