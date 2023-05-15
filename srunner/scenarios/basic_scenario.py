f8cfb2f9c7927a293a75e7be49d1445185750678
Songyang Yan <yan_sy@126.com>
2023-05-15 16:08:02 +0800

    fix duplicate env initialization during runtime

--- srunner/scenarios/basic_scenario.py
+++ srunner/scenarios/basic_scenario.py
@@ -57,7 +57,10 @@ class BasicScenario(object):
         if debug_mode:
             py_trees.logging.level = py_trees.logging.Level.DEBUG
 
-        self._initialize_environment(world)
+        if self.route_mode:
+            # Only init env for route mode, avoid duplicate initialization during runtime
+            self._initialize_environment(world)
+            
         self._initialize_actors(config)
 
         if CarlaDataProvider.is_sync_mode():
