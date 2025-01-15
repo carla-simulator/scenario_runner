c2bf0098ae3f6565fd9171ef5e58f93c6049bcad
dmholtz <56723830+dmholtz@users.noreply.github.com>
2025-01-15 20:26:33 +0100

    Revert "Added log agent"
    This reverts commit 5828e5913d7bace51de56d8516ca6c6190213acb.

--- srunner/scenariomanager/scenarioatomics/atomic_behaviors.py
+++ srunner/scenariomanager/scenarioatomics/atomic_behaviors.py
@@ -4425,7 +4425,7 @@ class ScenarioTriggerer(AtomicBehavior):
 
         # Check which scenarios can be triggered
         blackboard = py_trees.blackboard.Blackboard()
-        for black_var_name, scen_location, scen_name in self._blackboard_list:
+        for black_var_name, scen_location in self._blackboard_list:
 
             # Close enough
             scen_distance = route_location.distance(scen_location)
@@ -4441,8 +4441,6 @@ class ScenarioTriggerer(AtomicBehavior):
             if condition1 and condition2 and condition3:
                 _ = blackboard.set(black_var_name, True)
                 self._triggered_scenarios.append(black_var_name)
-
-                CarlaDataProvider.set_latest_scenario(scen_name)
 
                 if self._debug:
                     self._world.debug.draw_point(
